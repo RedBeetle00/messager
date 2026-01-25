d51c99cefdada1debbeec0dcfc96d9293cff89b2
redbeetle00 <redjuks@gmail.com>
2026-01-25 16:32:06 +0300

    Do not touch this working build

--- phoneside/app/src/main/java/com/appy/messager/MainActivity.kt
+++ phoneside/app/src/main/java/com/appy/messager/MainActivity.kt
@@ -5,32 +5,32 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.core.app.NotificationManagerCompat
-import kotlinx.coroutines.*
-import android.widget.Button
 
 
 class MainActivity : ComponentActivity() {
     private lateinit var tcpClient: TcpClient
-    private val scope = CoroutineScope(Dispatchers.Main + Job())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         tcpClient = TcpClient()
 
         setContentView(R.layout.layout)
 
-        //if (!isServiceEnabled(this)) {
-        //    val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
-        //    startActivity(intent)
-        //}
+        if (!isServiceEnabled(this)) {
+            val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+            startActivity(intent)
+        }
 
-        //val startButt = findViewById<Button>(R.id.startButton)
-        //startButt.setOnClickListener {
-        //    scope.launch {
-        //        val isConnected = tcpClient.connect("192.168.1.11", 8080)
-        //    }
-        //}
+//        val startButt = findViewById<Button>(R.id.startButton)
+//        startButt.setOnClickListener {
+//            isOn = true
+//            println("Start button, $notificationService.isOff")
+//        }
+//        val stopButt = findViewById<Button>(R.id.stopButton)
+//        stopButt.setOnClickListener {
+//            isOn = false
+//            println("Stop button, $notificationService.isOff")
+//        }
     }
 }
 
--- phoneside/app/src/main/java/com/appy/messager/NotificationService.kt
+++ phoneside/app/src/main/java/com/appy/messager/NotificationService.kt
@@ -8,27 +8,26 @@ import kotlinx.coroutines.launch
 
 class NotificationService : NotificationListenerService() {
     private lateinit var tcpClient: TcpClient
-
     private val scope = CoroutineScope(Dispatchers.Main + Job())
     override fun onNotificationPosted(sbn: StatusBarNotification) {
         tcpClient = TcpClient()
         scope.launch {
-            tcpClient.connect("192.168.1.11", 8080)
-            val scope = CoroutineScope(Dispatchers.Main + Job())
+            val connect = tcpClient.connect("192.168.1.11", 8080)
+            if (connect) {
+                // Получаем уведомление
+                val packageName = sbn.packageName
+                val notification = sbn.notification
 
-            // Получаем уведомление
-            val packageName = sbn.packageName
-            val notification = sbn.notification
+                // Извлекаем данные
+                val title = notification.extras.getString(Notification.EXTRA_TITLE)
+                val text = notification.extras.getString(Notification.EXTRA_TEXT)
+                val subText = notification.extras.getString(Notification.EXTRA_SUB_TEXT)
 
-            // Извлекаем данные
-            val title = notification.extras.getString(Notification.EXTRA_TITLE)
-            val text = notification.extras.getString(Notification.EXTRA_TEXT)
-            val subText = notification.extras.getString(Notification.EXTRA_SUB_TEXT)
+                // Отправляем данные на ПК
+                tcpClient.sendMessage("$title:\n$text\n")
 
-            // Отправляем данные на ПК
-            tcpClient.sendMessage("$title:\n$text\n")
-
-            tcpClient.disconnect()
+                tcpClient.disconnect()
+            }
         }
     }
 }
